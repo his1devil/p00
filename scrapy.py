ca508c5cf17c922d975316840fcd4524d4023146
@antai <evilat@outlook.com>
2017-01-12 18:54:00 +0800

    UPDATED

--- scrapy.py
+++ scrapy.py
@@ -6,7 +6,7 @@ try:
     import simplejson as json
 except ImportError:
     import json
-from collections import OrderedDict
+# from collections import OrderedDict new in 2.7 ;(
 
 
 class Config(object):
@@ -34,19 +34,24 @@ def running_command(cmd):
 
 
 class DBInfo(object):
-    def __init__(self, db_runner, socket, host='localhost'):
+    def __init__(self, db_runner, socket, user, passwd, host='localhost'):
         self.db_runner = db_runner
         self.socket = socket
+        self.user = user
+        self.passwd = passwd
         self.host = host
 
     @staticmethod
     def get_mem():
-        meminfo = OrderedDict()
+        meminfo = {}
         with open('/proc/meminfo') as f:
             for line in f:
                 if line.startswith(('MemTotal', 'MemFree')):
                     meminfo[line.split(':')[0]] = line.split(':')[1].strip()
         return meminfo
+
+    def get_db_ms_status(self, port):
+        pass
 
     def get_db_status(self, db_type='mysql'):
         if self.db_runner:
@@ -54,7 +59,14 @@ class DBInfo(object):
         else:
             cmd = "ps aux|grep {0}|grep -v grep".format(db_type)
         data = running_command(cmd)
-        return data
+        instances = []
+        for line in data:
+            response_json = {}
+            for i in line.split("--"):
+                if i.startswith(('datadir', 'user', 'socket', 'port')):
+                    response_json[i.split('=')[0]] = i.split('=')[1]
+            instances.append(response_json)
+        return instances
 
     # def get_ms_status(self, cmd, fields):
     #     if data:
@@ -64,14 +76,30 @@ class DBInfo(object):
     #     return results
 
 
+class Instance(DBInfo):
+    def __init__(self):
+        super(Instance, self).__init__()
+        self.port = port
+        self.datadir = datadir
+        self.socket = socket
+
+    def get_datadir_size(self):
+        free = running_command("df -h {0}".format(self.datadir))[1].split()[3]
+        used = running_command("du -sh {0}".format(self.datadir)).split()[0]
+        return used, free
+
+    def get_ms_status(self, cmd, fields):
+        cmd = "echo 'show slave status\G;'|mysql -u{0} -p{1} {2} {3}".format('root', '123', '-S', self.socket)
+
+
 if __name__ == '__main__':
     cfg = Config('configure')
 
     # 数据库 运行用户 / 状态
     runner = cfg.get('mysql_runner', 'user')
     sock = cfg.get('start', 'socket')
-    results = DBInfo(runner, sock)
-    print results.get_db_status()
+    info = DBInfo(runner, sock)
+    print info.get_db_status()
 
     # 数据库M/S状态
     db_status = tuple([v.strip() for v in cfg.get('fields', 'status').split(',')])
@@ -87,10 +115,3 @@ if __name__ == '__main__':
     # # 获取服务器内存状态
     # server = DBInfo()
     # print "Mem:", server.get_mem()
-    #
-    # # 数据库运行用户
-    #
-    # df = subprocess.Popen(["df", "filename"], stdout=subprocess.PIPE)
-    # output = df.communicate()[0]
-    # device, size, used, available, percent, mountpoint = \
-    #     output.split("\n")[1].split()\
